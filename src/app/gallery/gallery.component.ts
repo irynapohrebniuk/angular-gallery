7c7961710b61324e77696d0dfd8856d1b6049c5c
Iryna Pohrebniuk <iryna.pohrebniuk@gmail.com>
2020-05-29 23:50:55 +0200

    Add effect slideshow to the gallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -12,16 +12,26 @@ export class GalleryComponent {
     'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
     'https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
     'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    'https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
+    'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    'https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
+    'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    'https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
+    'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
   ]
 
   currentIndex = 0;
 
   constructor() {
-    // let i = 0;
-    // while (i < this.pictures.length) {
-    //   setInterval(()=> {
-    //   this.currentIndex = this.currentIndex + 1;
-    // }, 3000)
+    setInterval(() => {
+      const n = this.pictures.length;
+      this.currentIndex = this.currentIndex + 1;
+      if (this.currentIndex === n - 1) { this.currentIndex = 0 }
+      console.log("currentIndex", this.currentIndex);
+    }, 2000);
   }
 
   handleClick(index) {
@@ -32,14 +42,14 @@ export class GalleryComponent {
     if (this.currentIndex > 0) {
       this.currentIndex = this.currentIndex - 1;
     }
-    
+
   }
   handleNext() {
-    
-    if (this.currentIndex < this.pictures.length -1 ) {
+
+    if (this.currentIndex < this.pictures.length - 1) {
       this.currentIndex = this.currentIndex + 1;
     }
-    
+
   }
-    
+
 }
